b9e5780ab6c6799b7cc381bf0d1de72f3ac3cece
Oleg Kopach <105640691+OlegKopach@users.noreply.github.com>
2022-07-17 18:34:18 +0300

    MyJob
    All

---
+++ HW1/HW1/Program.cs
@@ -0,0 +1,35 @@
+﻿/*Development and Build Tools 
+ Практическое задание
+Написать программу, которая принимает из командной строки последовательность символов (строку) в качестве аргумента 
+и выводит в консоль максимальное количество неодинаковых последовательных символов в строке.*/
+namespace HW1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            String str = "Have a good day";
+            int result = 0;
+
+            do
+            {
+                for (int i = 0; i < str.Length; i++)
+                {
+                    if (str.IndexOf(str[i]) < i)
+                    {
+                        if (i > result)
+                        {
+                            result = i;
+                        }
+                        str = str.Substring(str.IndexOf(str[i]) + 1);
+                        break;
+                    }
+                }
+            } while (str.Length > result);
+
+            Console.WriteLine(result);
+        }
+
+
+    }
+}
